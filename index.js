5663d1db3baac23de5269f1ff54f393e71b73143
Nimesh Solanki <nimesh3536@gmail.com>
2020-03-30 02:46:28 +0530

    limit new sms sent

--- index.js
+++ index.js
@@ -14,36 +14,40 @@ const {
 const interval = 1000 * 60 * 5; // 5 minutes
 
 const TwilioClient = new twilio(accountSid, authToken);
-
+let previousSlot = 0;
 
 function GetStatus() {
   axios
     .post(bbURL, body, { headers: headers })
     .then(res => {
       const slot = res.data.response.selected_address.slot;
-      if(slot.includes("All Slots Full")) {
-          console.log(slot);
-      }
-      else {
+      if (slot.includes('All Slots Full')) {
+        console.log(slot);
+      } else {
+        if (previousSlot === slot) {
+          console.log(`Same Slot: ${slot}`);
+        } else {
           console.log(`slot: ${slot}.`);
-            TwilioClient.messages
-              .create({
-                body: slot,
-                to: ToNumber,
-                from: FromNumber
-              })
-              .then(res => {
-                console.log(res.sid);
-              })
-              .catch(e => {
-                console.log(`Twilio Error`);
-                console.log(e);
-              });
+          TwilioClient.messages
+            .create({
+              body: slot,
+              to: ToNumber,
+              from: FromNumber
+            })
+            .then(res => {
+              previousSlot = slot;
+              console.log(res.sid);
+            })
+            .catch(e => {
+              console.log(`Twilio Error`);
+              console.log(e);
+            });
+        }
       }
     })
     .catch(e => {
       console.error(e);
     });
 }
-setInterval(GetStatus,interval);
+setInterval(GetStatus, interval);
 // GetStatus();
