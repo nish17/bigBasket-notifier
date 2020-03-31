769e021720c6301d93c0141c8275488c4f839ec0
Nimesh Solanki <nimesh3536@gmail.com>
2020-04-01 00:24:56 +0530

    make it simple

--- index.js
+++ index.js
@@ -14,40 +14,35 @@ const {
 const interval = 1000 * 60 * 5; // 5 minutes
 
 const TwilioClient = new twilio(accountSid, authToken);
-let previousSlot = 0;
-
-function GetStatus() {
-  axios
-    .post(bbURL, body, { headers: headers })
-    .then(res => {
-      const slot = res.data.response.selected_address.slot;
-      if (slot.includes('All Slots Full')) {
-        console.log(slot);
-      } else {
-        if (previousSlot === slot) {
-          console.log(`Same Slot: ${slot}`);
-        } else {
-          console.log(`slot: ${slot}.`);
-          TwilioClient.messages
-            .create({
-              body: slot,
-              to: ToNumber,
-              from: FromNumber
-            })
-            .then(res => {
-              previousSlot = slot;
-              console.log(res.sid);
-            })
-            .catch(e => {
-              console.log(`Twilio Error`);
-              console.log(e);
-            });
-        }
+let previousSlot = '', slot = '';
+async function GetStatus() {
+  try {
+    const res = await axios.post(bbURL, body, { headers: headers });
+    slot = res.data.response.selected_address.slot;
+  }
+  catch(err){
+    console.error(err);
+  }
+  if (slot.includes('All Slots Full')) {
+    console.log(slot);
+  } else {
+    if (previousSlot === slot) {
+      console.log(`Same Slot: ${slot}`);
+    } else {
+      previousSlot = slot;
+      console.log(`slot: ${slot}.`);
+      try {
+        const message = await TwilioClient.messages.create({ body: slot, to: ToNumber, from: FromNumber})
+          console.log(message);
+      } catch(err) {
+        console.log(`Twilio Error`);
+        console.log(e);
       }
-    })
-    .catch(e => {
-      console.error(e);
-    });
+    }
+  }
 }
-setInterval(GetStatus, interval);
-// GetStatus();
+
+exports.handler = async function(event, context) {
+  // setInterval(GetStatus, interval);
+  await GetStatus();
+};
